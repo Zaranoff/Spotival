8281d60f147772c14479ed111add224ccc26380f
ValentinG <valentin.gardie@viacesi.fr>
2021-04-11 20:42:56 +0200

    Barre navigation

--- Spotival/visuals/MainWindow.xaml.cs
+++ Spotival/visuals/MainWindow.xaml.cs
@@ -24,5 +24,57 @@ namespace Spotival
         {
             InitializeComponent();
         }
+
+        public void SelectFunction(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            btnDeezer.Background = new SolidColorBrush(Color.FromRgb(48,48,48));
+            btnExplorer.Background = new SolidColorBrush(Color.FromRgb(48, 48, 48));
+            btnHome.Background = new SolidColorBrush(Color.FromRgb(48, 48, 48));
+            btnSettings.Background = new SolidColorBrush(Color.FromRgb(48, 48, 48));
+            btnSpotify.Background = new SolidColorBrush(Color.FromRgb(48, 48, 48));
+
+            switch (btn.Name)
+            {
+                case "btnDeezer":
+                    btn.Background = new SolidColorBrush(Color.FromRgb(213, 78, 33));
+                    break;
+                case "btnExplorer":
+                    btn.Background = new SolidColorBrush(Color.FromRgb(213, 78, 33));
+                    break;
+                case "btnHome":
+                    btn.Background = new SolidColorBrush(Color.FromRgb(213, 78, 33));
+                    break;
+                case "btnSettings":
+                    btn.Background = new SolidColorBrush(Color.FromRgb(213, 78, 33));
+                    break;
+                case "btnSpotify":
+                    btn.Background = new SolidColorBrush(Color.FromRgb(213, 78, 33));
+                    btn. = new BitmapImage(new Uri("/Application;component/resources/spotify_green.png", UriKind.Relative));
+                    break;
+            }
+        }
+
+        public void CloseWindow(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void MinimizeWindow(object sender, EventArgs e)
+        {
+           WindowState = WindowState.Minimized;
+        }
+
+        public void MaximizeWindow(object sender, EventArgs e)
+        {
+            if(WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
     }
 }
